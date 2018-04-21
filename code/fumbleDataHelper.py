ed36f122a316fe2740a6feef108f6dab1f7fa5c2
richhildebrand <richhildebrand@gmail.com>
2018-04-21 16:47:19 -0400

    calculate fumbles per game

--- code/fumbleDataHelper.py
+++ code/fumbleDataHelper.py
@@ -5,8 +5,8 @@ def updatePlayersInCollection(collection):
     for rowNumber, rowData, in collection.items():
         updatePlayerInCollection(rowData, combinedFumbleData)
 
+    calculateFumblesPerGame(combinedFumbleData)
     return combinedFumbleData
-
 
 def updatePlayerInCollection(row, allPlayers):
     playerId = row['Player Id']
@@ -19,3 +19,19 @@ def updatePlayerInCollection(row, allPlayers):
 
     allPlayers[playerId] = player
     return
+
+def calculateFumblesPerGame(combinedFumbleData):
+    for key, playerData, in combinedFumbleData.items():
+        gamesPlayed = playerData['Games Played']
+        fumbles = playerData['Fumbles']
+        fumblesPerGame = safeZeroDivision(gamesPlayed, fumbles)
+
+        playerData['Fumbles Per Game'] = fumblesPerGame
+
+    return
+
+def safeZeroDivision(numerator, denominator):
+    try:
+        return numerator / denominator
+    except ZeroDivisionError:
+        return 0
