47e3d71e830d2c17018c808978f7c1c8573a5a50
richhildebrand <richhildebrand@gmail.com>
2018-04-21 11:36:31 -0400

    import some csv data

--- code/main.py
+++ code/main.py
@@ -1,1 +1,13 @@
-print ("Print!")
+print ('Starting import')
+
+import csv
+
+with open('../stats/Career_Stats_Fumbles.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for i,row in enumerate(reader):
+        print(row['Name'], row['Position'], row['Fumbles'])
+        if(i >= 9):
+            break
+
+
+print ('Ending import')
