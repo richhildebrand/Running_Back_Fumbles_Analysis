1f387d5d836e8cb618a9eb4fa2a967945f00e2da
richhildebrand <richhildebrand@gmail.com>
2018-04-22 11:31:09 -0400

    read all rows

--- tests/csvHelperShould/test_readRowsFromShould.py
+++ tests/csvHelperShould/test_readRowsFromShould.py
@@ -10,5 +10,13 @@ class ReadRowsFromShould(unittest.TestCase):
         rows = csvHelper.readRowsFrom(2, 'tests/testHelpers/testData/SampleData.csv')
         assert len(rows) is 2
 
+    def test_ReadFiveRows(self):
+        rows = csvHelper.readRowsFrom(5, 'tests/testHelpers/testData/SampleData.csv')
+        assert len(rows) is 5
+
+    def test_ReadAllRows(self):
+        rows = csvHelper.readRowsFrom(1000, 'tests/testHelpers/testData/SampleData.csv')
+        assert len(rows) is 6
+
 if __name__ == '__main__':
     unittest.main()
